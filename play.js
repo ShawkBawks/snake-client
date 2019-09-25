efab8c7181ae5cb85ca6c88b6f74a8fdb10deef1
Logan Roberts <lroberts77@gmail.com>
2019-09-25 22:20:26 +0000

    set up movement commands

--- play.js
+++ play.js
@@ -1,15 +1,8 @@
 const { connect } = require('./client');
 const { setupInput } = require('./input');
+
+
 console.log('Connecting ...');
-connect();
-
-setupInput();
-// handleUserInput();
-// const handleUserInput = function(data) {
-//   if (key === '\u0003') {
-//     process.exit();
-//   }
-// }
-
-// on any input from stdin (standard input), output a "." to stdout
+let conn = connect();
+setupInput(conn);
 
