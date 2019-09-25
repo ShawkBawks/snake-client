27aac19db82009b9eae69ba057cc387328eba695
Logan Roberts <lroberts77@gmail.com>
2019-09-25 21:51:40 +0000

    exported additional modules for input

--- play.js
+++ play.js
@@ -1,26 +1,10 @@
 const { connect } = require('./client');
-
+const { setupInput } = require('./input');
 console.log('Connecting ...');
 connect();
-const handleUserInput = function (data) {
-  process.stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  })
-};
 
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', (data) => { handleUserInput(data) });
-  return stdin;
-};
 setupInput();
-handleUserInput();
+// handleUserInput();
 // const handleUserInput = function(data) {
 //   if (key === '\u0003') {
 //     process.exit();
