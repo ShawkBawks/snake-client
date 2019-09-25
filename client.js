6211122409816441fe7503f9c73338c83347ec19
Logan Roberts <lroberts77@gmail.com>
2019-09-25 20:47:03 +0000

    Added initials!

--- client.js
+++ client.js
@@ -1,17 +1,23 @@
 const net = require('net');
 
-const connect = function() {
-  const conn = net.createConnection({ 
+const connect = function () {
+  const conn = net.createConnection({
     host: '192.168.88.254',
     port: 50541
   });
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server!')
+    conn.write("Name: LTR");
+  });
+
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
-// interpret incoming data as text
-conn.setEncoding('utf8'); 
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
 
-return conn;
+  return conn;
 }
 
-module.exports = {connect};
+module.exports = { connect };
